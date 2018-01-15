65d82c532bbdaaa728a6f1d981128652b170c46a
bottleworks <5555428+bottleworks@users.noreply.github.com>
2018-01-15 02:01:35 -0500

    Add files via upload

--- Reset Device/reset.py
+++ Reset Device/reset.py
@@ -17,7 +17,7 @@ while True:
         if counter == 9:
             os.system('sudo mv /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf.OLD')
             os.system('sudo rm -f /usr/share/configure_wifi/tmp/*')
-            os.system('sudo cp /usr/share/configure_wifi/Reset_Device/static_files/dhcpcd.conf /etc/')
+            os.system('sudo cp /usr/share/configure_wifi/Reset_Device/static_files/dhcpcd.conf.aphost /etc/dhcpcd.conf')
             os.system('sudo cp /usr/share/configure_wifi/Reset_Device/static_files/hostapd.conf /etc/hostapd/')
             os.system('sudo cp /usr/share/configure_wifi/Reset_Device/static_files/dnsmasq.conf /etc/dnsmasq.conf')
             os.system('sudo cp /usr/share/configure_wifi/Reset_Device/static_files/default_hostapd /etc/default/hostapd')
