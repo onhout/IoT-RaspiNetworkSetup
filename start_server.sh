ec14d7c2b4f91d52e74d8ee98a29f2e1dcaecdcb
bottleworks <5555428+bottleworks@users.noreply.github.com>
2018-01-17 21:15:24 +0000

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -4,5 +4,5 @@ while true
   do
     sudo su -c "cd /usr/share/configure_wifi/Configuration_App/ && rails s -b 10.0.0.1 -e production -p 80 -d"
     #cd /usr/share/configure_wifi/Configuration_App/ && /usr/local/bin/rails s -b 10.0.0.1 -e production -p 80 -d
-    sleep 20
+    sleep 60
   done
