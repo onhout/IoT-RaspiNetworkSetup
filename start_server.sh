28852c7c9ca58a6776d6d1ebb699b773b6781a4f
bottleworks <5555428+bottleworks@users.noreply.github.com>
2018-01-17 21:14:50 +0000

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,8 @@
-#!/bin/sh -e
+#! /bin/sh
 
-# Start the WiFi Configure script
-# su -c "cd /usr/share/configure_wifi/Configuration_App/ && rails s -b 10.0.0.1 -e production -p 80 -d"
-cd /usr/share/configure_wifi/Configuration_App/ && /usr/local/bin/rails s -b 10.0.0.1 -e production -p 80 -d
+while true
+  do
+    sudo su -c "cd /usr/share/configure_wifi/Configuration_App/ && rails s -b 10.0.0.1 -e production -p 80 -d"
+    #cd /usr/share/configure_wifi/Configuration_App/ && /usr/local/bin/rails s -b 10.0.0.1 -e production -p 80 -d
+    sleep 20
+  done
