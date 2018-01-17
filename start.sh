555237a2c1676f211e8572e352e6f41a89ef87af
bottleworks <5555428+bottleworks@users.noreply.github.com>
2018-01-17 21:23:39 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ mv "Configuration App" Configuration_App
 mv "Reset Device" Reset_Device
 rm -rf /usr/share/configure_wifi
 mkdir /usr/share/configure_wifi
-cp -r * /usr/share/configure_wifi
 chmod 775 /usr/share/configure_wifi
+cp -r * /usr/share/configure_wifi
 echo "Starting Main installer script..."
 python3 /usr/share/configure_wifi/initial_setup.py
