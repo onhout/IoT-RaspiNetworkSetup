9bff12ad1aa22ed7c26b774b4b83f792224dbff2
MAKOMO <marko.luther@gmx.net>
2018-01-18 18:05:18 +0100

    the script start.sh needs to be executable to allow the Rails server to startup

--- start.sh
+++ start.sh
@@ -6,5 +6,6 @@ rm -rf /usr/share/configure_wifi
 mkdir /usr/share/configure_wifi
 chmod 775 /usr/share/configure_wifi
 cp -r * /usr/share/configure_wifi
+chmod 775 /usr/share/configure_wifi/start_server.sh
 echo "Starting Main installer script..."
 python3 /usr/share/configure_wifi/initial_setup.py
